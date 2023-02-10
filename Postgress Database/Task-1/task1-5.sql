417a790a5bc9ebcd887450e3a74cb681e97f7e95
Nourhan Hesham <117953315+nourhanHesham77@users.noreply.github.com>
2023-02-10 22:41:05 +0200

    Update task1-5.sql

--- Postgress Database/Task-1/task1-5.sql
+++ Postgress Database/Task-1/task1-5.sql
@@ -1,14 +1,14 @@
--- task1
+-- task1..https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
 SELECT * FROM CITY WHERE ID = 1661;
 
--- task2
+-- task2..https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
 SELECT CITY,STATE FROM STATION;
 
---task3
+--task3..https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
 SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE='USA';
 
---task-4
+--task-4..https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
 SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
 
---task-5
+--task-5..https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
 SELECT COUNT(CITY)-COUNT(DISTINCT CITY) FROM STATION;
