f581033571fb8620ce1eb495518f3b9d3d5b970d
Nourhan Hesham <117953315+nourhanHesham77@users.noreply.github.com>
2023-02-22 15:48:53 +0200

    Tasks Done (MYSQL)

--- Postgress Database/Task-3/weather-observation-station-2.sql
+++ Postgress Database/Task-3/weather-observation-station-2.sql
@@ -1,3 +1,6 @@
---p7..https://www.hackerrank.com/challenges/weather-observation-station-2/problem?isFullScreen=true
---i used cast to throw zeros after 2 dec.
+-- p7..https://www.hackerrank.com/challenges/weather-observation-station-2/problem?isFullScreen=true
+-- i used cast to throw zeros after 2 dec. in(db)
 SELECT CAST(ROUND(SUM(LAT_N), 2)AS NUMERIC(12,2)) AS lat, CAST(ROUND(SUM(LONG_W), 2) AS NUMERIC(12,2)) AS lan from STATION;
+
+-- (MYSQL)
+SELECT ROUND(SUM(LAT_N), 2) AS lat, ROUND(SUM(LONG_W), 2) AS lan from STATION;
--- Postgress Database/Task-4/what-type-of-triangle.sql
+++ Postgress Database/Task-4/what-type-of-triangle.sql
@@ -1,9 +1,7 @@
---p4..https://www.hackerrank.com/challenges/what-type-of-triangle/problem?isFullScreen=true
---Note about this: checking not triangle first then the rest, or put the 'not triangle' condition with all of them
+-- p4..https://www.hackerrank.com/challenges/what-type-of-triangle/problem?isFullScreen=true
+-- Note about this: checking not triangle first then the rest, or put the 'not triangle' condition with all of them
 
-SELECT CASE WHEN (A+B <= C) OR (A+C <= B) OR (B+C <= A) THEN 'Not A Triangle'
-WHEN A=B AND B=C THEN 'Equilateral'
-WHEN ((A=B) AND (A!=C)) OR ((A=C) AND (A !=B)) OR ((B=C) AND (B!=A)) THEN 'Isosceles'
-WHEN A!=B AND A!=C AND B!=C THEN 'Scalene'
-END
+
+-- (MYsql)
+SELECT CASE WHEN (A+B <= C OR A+C <= B OR B+C <= A) THEN 'Not A Triangle' WHEN A=B AND B=C THEN 'Equilateral' WHEN ((A=B) AND (A!=C)) OR ((A=C) AND (A !=B)) OR ((B=C) AND (B!=A)) THEN 'Isosceles' ELSE 'Scalene' END
 FROM TRIANGLES;
