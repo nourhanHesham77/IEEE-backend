2af7650b06243b5046ef9e608c893989e88a2f91
Nourhan Hesham <117953315+nourhanHesham77@users.noreply.github.com>
2023-02-22 14:41:16 +0200

    Postgress Database Project(Mysql)

--- Project-1/harry-potter-and-wands.sql
+++ Project-1/harry-potter-and-wands.sql
@@ -1,7 +1,7 @@
 --Medium(Intermediate)....https://www.hackerrank.com/challenges/harry-potter-and-wands/problem?isFullScreen=true
-
-SELECT W1.id,P.age,W1.coins_needed,W1.power FROM Wands AS W1
+--(MySQL)
+SELECT W1.id, P.age, W1.coins_needed, W1.power FROM Wands AS W1
 JOIN Wands_Property AS P ON P.code=W1.code
 WHERE P.is_evil = 0 AND (W1.coins_needed) = (SELECT MIN(W.coins_needed) FROM Wands AS W 
-JOIN Wands_Property AS P2 ON P2.code=W.code AND P2.age=P.age AND P2.is_evil=0 AND W1.power=W.power )
+JOIN Wands_Property AS P2 ON P2.code=W.code WHERE P2.age=P.age AND P2.is_evil=0 AND W1.power=W.power )
  ORDER BY W1.power DESC ,P.age DESC;
--- Project-1/the-pads.sql
+++ Project-1/the-pads.sql
@@ -1,9 +1,6 @@
 --Medium(Basic)..https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
 
-
--- SELECT name || '(' || substr(occupation,1,1) || ')' AS Occupation_Name FROM Occupations ORDER BY Occupation_Name ASC;
-
-SELECT Name || '(' || LEFT(occupation,1) || ')'  AS lat FROM occupations
-ORDER BY lat ASC;
---note..(GROUP BY)
-SELECT 'There are a total of '||COUNT(occupation),LOWER(occupation)||'s.' FROM occupations GROUP BY occupation  ORDER BY COUNT(occupation), LOWER(occupation);
+--(MYSQL)
+SELECT CONCAT(Name,'(',LEFT(occupation,1),')')  AS lat FROM occupations
+UNION
+SELECT CONCAT('There are a total of ',COUNT(Occupation),' ',LOWER(Occupation),'s.') AS lat  FROM occupations GROUP BY occupation  ORDER BY lat ASC;
